151e3c12d86d05c0009ed63c3550abaaf066c134
eZWALT <walterthehshadow333@gmail.com>
2023-03-11 11:24:11 +0100

    mamasita v8: heuristic mamasita unified

--- PRACTICA1/src/blablacar/BlaBlaHeuristicFunctions.java
+++ PRACTICA1/src/blablacar/BlaBlaHeuristicFunctions.java
@@ -1,34 +1,84 @@
-//package src.blablacar; DONA ERROR I NO SE PERQUE PUTU JAVA
+package src.blablacar;
 
 import java.util.*;
 import aima.search.framework.HeuristicFunction;
+import src.BlaBlaEstado;
+
+import java.lang.IllegalStateException;
 /**
  * @author Walter J.T.V
  */
-public class BlaBlaHeuristicFunction implements HeuristicFunction{
+public class BlaBlaHeuristicFunctions implements HeuristicFunction{
 
     
     /**
      * @param state This is a non-instanciated state 
-     * @return Total number of cars
+     * @param Choice This number determines which heuristic is gonna be used, from heuristic1 to heuristic 8. Throws exception
+     * @throws IllegalStateException if the number provided is not between 1-8 then this exception is thrown
+     * @return Depending on the function chosen, the value returned will be 
+     *    O(1) 1. Total Number of cars  
+     *    O(1) 2. Total Number of cars squared
+     *    O(n) 3. Sum of the length of the paths to work for each user 
+     *    O(n) 4. Sum of the length of the paths to work for each user squared
+     *    O(n) 5. Average of the length of the path to work for each user
+     *    O(n) 6. Average of the length of the path to work for each user (each element squared)
+     *    O(n) 7. Entropy of the length of path to work from each user
+     *    O(n) 8. Entropy of the length of path to work from each user multiplied by the number of cars
      */
-    public double getHeuristicValue(int state,int Choice){
+    public double getHeuristicValue(Object State,int Choice) throws IllegalStateException{
+
+        src.blablacar.BlaBlaEstado estado = (BlaBlaEstado) State;
+        double NumberOfCars = estado.num_conductores();
+        ArrayList<Integer> PathLengths = estado.distancias_coches();
+        int n = PathLengths.size();
+        double h = 0;
 
         switch (Choice) {
             case 1:
-            
-                double numberofcars = state; //this needs to be updated
-                return numberofcars;
+
+                h = NumberOfCars;
                 break;
             case 2:
 
-                double numberofcars = state; //this needs to be updated
-                return numberofcars * numberofcars;
+                h = NumberOfCars*NumberOfCars;
+                break;
             case 3:
 
-            default:
+                for(int i = 0; i < n; ++i) h += PathLengths(i);   
+                break; 
+            case 4:
+
+                for(int i = 0; i < n; ++i) h += PathLengths(i) * PathLengths(i);
                 break;
+
+            case 5:
+
+                for(int i = 0; i < n; ++i) h += PathLengths(i);
+                h = h / (double)n;
+                break;
+            
+            case 6:
+
+                for(int i = 0; i < n; ++i) h += PathLengths(i) * PathLengths(i);
+                h = h / (double)n;
+                break;
+
+            case 7: 
+
+                for(int i = 0; i < n; ++i) h += PathLengths(i);
+                 h = h * Math.log(h);
+
+            case 8:
+                for(int i = 0; i < n; ++i) h += PathLengths(i);
+                h = h * Math.log(h) * NumberOfCars;
+
+            default:
+                throw new IllegalStateException("maricones usad el numero entre 1 y 8 joder");
+                break;
+
         }
+
+        return h;
     }
 
     /**
